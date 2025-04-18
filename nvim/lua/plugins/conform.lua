5343e29e55b436ce13af593b5f9f8d9906b331da
david_doichita <daviddoichita@proton.me>
2025-04-18 17:19:44 +0200

    Update neovim dotfiles

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,16 +1,18 @@
 return {
-	'stevearc/conform.nvim',
-	config = function ()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
-		})
-	end
+    'stevearc/conform.nvim',
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                typescript = { "prettierd", "prettier", stop_after_first = true },
+                typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        })
+    end
 }
---
+++ nvim/lua/plugins/md-links.lua
@@ -0,0 +1,9 @@
+return {
+    'Nedra1998/nvim-mdlink',
+    config = function()
+        require('nvim-mdlink').setup({
+            keymap = true,
+            cmp = true
+        })
+    end
+}
